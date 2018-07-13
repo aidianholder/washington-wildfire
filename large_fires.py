3adcc036e7a890a562806ce9c84b06c07dd51782
Aidian Holder <aidianholder@gmail.com>
2018-07-13 06:24:11 -0700

    Perims, incidents downoad working

--- large_fires.py
+++ large_fires.py
@@ -1,7 +1,18 @@
 #! /usr/local/bin/env python
 
 from arcgis import ArcGIS
+from geojson import dumps
 
-NCGCC_layers = ArcGIS("https://services3.arcgis.com/T4QMspbfLg3qTGWY/ArcGIS/rest/services/NWCC_Operational_Layers/FeatureServer")
+NWGCC_layers = ArcGIS("https://services3.arcgis.com/T4QMspbfLg3qTGWY/ArcGIS/rest/services/NWCC_Operational_Layers/FeatureServer")
 
-large_fires = NWGCC.get(1)
+large_fires = NWGCC_layers.get(1)
+
+outfile = open('large_incidents.geojson', 'w')
+outfile.write(dumps(large_fires))
+
+perimeters = NWGCC_layers.get(2)
+
+outfile = open('current_perimeters.geojson', 'w')
+outfile.write(dumps(perimeters))
+
+#def esri_download(service, layer_num, layer_name, file_name)
