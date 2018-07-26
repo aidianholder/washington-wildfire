c18e1a43d1bc04701b6d37e2fb1fc1bb58eea675
Aidian Holder <aidianholder@gmail.com>
2018-07-26 16:30:23 -0700

    working on composting data

--- large_fires.py
+++ large_fires.py
@@ -1,44 +1,60 @@
 #! /usr/local/bin/env python
 
 from arcgis import ArcGIS
-from geojson import dumps
+from geojson import dumps, FeatureCollection
 
 NWGCC_layers = ArcGIS("https://services3.arcgis.com/T4QMspbfLg3qTGWY/ArcGIS/rest/services/NWCC_Operational_Layers/FeatureServer")
 
-large_fires = NWGCC_layers.get(1)
-fire_list = large_fires['features']
-for fire in fire_list:
 
 
-outfile = open('large_incidents.geojson', 'w')
-outfile.write(dumps(large_fires))
 
-perimeters = NWGCC_layers.get(2)
+#outfile = open('large_incidents.geojson', 'w')
+#outfile.write(dumps(large_fires))
 
-outfile = open('current_perimeters.geojson', 'w')
-outfile.write(dumps(perimeters))
+#perimeters = NWGCC_layers.get(2)
+
+#outfile = open('current_perimeters.geojson', 'w')
+#outfile.write(dumps(perimeters))
 
 #def esri_download(service, layer_num, layer_name, file_name)
 
 
-import requests
-from bs4 import BeautifulSoup
+def compose_fire_details():
+    import requests
+    from bs4 import BeautifulSoup
 
-r = requests.get('https://gacc.nifc.gov/nwcc/information/fire_info.aspx')
-t = r.text
-soup = BeautifulSoup(t)
-a = soup.find_all(class_="accordion")
-for incident in a:
-    fire_hash = {}
-    tables = incident.find_all("table")
-    for tb in tables:
-        f = tb.find_all("td")
-        for c in f:
-            k = c.find("label").string
-            v = c.find("span").string
-            fire_hash[k] = v
-            
-class wildfire:
-    def __init__(self, fire_nm):
-        self.fire_nm = fire_nm
+    fire_info = {}
+
+    r = requests.get('https://gacc.nifc.gov/nwcc/information/fire_info.aspx')
+    t = r.text
+    soup = BeautifulSoup(t)
+    a = soup.find_all(class_="accordion")
+    for incident in a:
+        fire_hash = {}
+        tables = incident.find_all("table")
+        for tb in tables:
+            f = tb.find_all("td")
+            for c in f:
+                k = c.find("label").string
+                v = c.find("span").string
+                fire_hash[k] = v
+        #print(fire_hash)
+        fire_info[fire_hash["Incident Name"]] = fire_hash
+        #print(fire_info)
+
+    large_fires = NWGCC_layers.get(1)
+    fire_list = large_fires['features']
+    for fire in fire_list:
+        new_details = fire_info.get(fire['properties']['FIRE_NM'])
+        print("large fire")
+        print(fire)
+        print("new details")
+        print(new_details)
+                
+compose_fire_details() 
+
+
+    
+
+
         
