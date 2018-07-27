901c543e939b9dc9576be642295cd3c5fb224224
Aidian Holder <aidianholder@gmail.com>
2018-07-26 21:32:03 -0700

    i hate git

--- large_fires.py
+++ large_fires.py
@@ -1,29 +1,27 @@
 #! /usr/local/bin/env python
 
 from arcgis import ArcGIS
-from geojson import dumps, FeatureCollection
+from geojson import dumps
+
 
 NWGCC_layers = ArcGIS("https://services3.arcgis.com/T4QMspbfLg3qTGWY/ArcGIS/rest/services/NWCC_Operational_Layers/FeatureServer")
 
 
 
 
-#outfile = open('large_incidents.geojson', 'w')
-#outfile.write(dumps(large_fires))
+outfile = open('large_incidents.geojson', 'w')
+outfile.write(dumps(large_fires))
 
-#perimeters = NWGCC_layers.get(2)
+perimeters = NWGCC_layers.get(2)
 
-#outfile = open('current_perimeters.geojson', 'w')
-#outfile.write(dumps(perimeters))
+outfile = open('current_perimeters.geojson', 'w')
+outfile.write(dumps(perimeters))
 
 #def esri_download(service, layer_num, layer_name, file_name)
 
 
-def compose_fire_details():
     import requests
     from bs4 import BeautifulSoup
-
-    fire_info = {}
 
     r = requests.get('https://gacc.nifc.gov/nwcc/information/fire_info.aspx')
     t = r.text
@@ -38,23 +36,8 @@ def compose_fire_details():
                 k = c.find("label").string
                 v = c.find("span").string
                 fire_hash[k] = v
-        #print(fire_hash)
-        fire_info[fire_hash["Incident Name"]] = fire_hash
-        #print(fire_info)
+
 
     large_fires = NWGCC_layers.get(1)
     fire_list = large_fires['features']
     for fire in fire_list:
-        new_details = fire_info.get(fire['properties']['FIRE_NM'])
-        print("large fire")
-        print(fire)
-        print("new details")
-        print(new_details)
-                
-compose_fire_details() 
-
-
-    
-
-
-        
